1f2c692d37c05825b781ba9630c01fdecc431860
Jordan Miguel <jordan.m.forde@gmail.com>
2016-07-30 23:18:07 +0100

    Update content.js
    Initially changed islamic to non-islamic. However a good Samaritan spotted that this can be implemented in a wrongful manner, for example turning 'islamic celebration of eid' into 'non-islamic celebration of eid' !! Thanks

--- content.js
+++ content.js
@@ -35,9 +35,10 @@ function handleText(textNode)
 	v = v.replace(/\bIslamic State\b/g, "Crazy Hate Group");
 	v = v.replace(/\bislamic state\b/g, "crazy hate group");
 	v = v.replace(/\bISIS\b/g, "CRAZY HATE GROUP");
-	v = v.replace(/\breligious\b/g, "non non religious");
-	v = v.replace(/\bIslam\b/g, "non islamic");
-	v = v.replace(/\bislam\b/g, "non islamic");
+	v = v.replace(/\breligious\b/g, "non-religious");
+	v = v.replace(/\bJihadist\b/g, "Non-Jihadist");
+	v = v.replace(/\bjihadist\b/g, "Non-Jihadist");
+	
 
 	textNode.nodeValue = v;
 }
