addda2e4409ea6945b6b60d09d1a2f37aa7bd4fe
Jordan Miguel <jordan.m.forde@gmail.com>
2017-01-28 13:27:02 +0000

    Update content.js
    Updating word replacement!

--- content.js
+++ content.js
@@ -36,19 +36,13 @@ function handleText(textNode)
 	v = v.replace(/\bIslamic-State\b/g, "Crazy Hate Group");
 	v = v.replace(/\bislamic-state\b/g, "Crazy Hate Group");
 	v = v.replace(/\bislamic state\b/g, "Crazy Hate Group");
-    v = v.replace(/\bislamic state\b/g, "Crazy Hate Group");
-	v = v.replace(/\bISIS\b/g, "Crazy Hate Group");
+        v = v.replace(/\bISIS\b/g, "Crazy Hate Group");
 	v = v.replace(/\bIsis\b/g, "Crazy Hate Group");
 	v = v.replace(/\bisis\b/g, "Crazy Hate Group");
-	v = v.replace(/\b(Isis)\b/g, "Crazy Hate Group");
-	v = v.replace(/\b(isis)\b/g, "Crazy Hate Group");
-	v = v.replace(/\b(ISIS)\b/g, "Crazy Hate Group");
-	v = v.replace(/\b(ISIS)\b/g, "Crazy Hate Group");
-	v = v.replace(/\bislamic militants\b/g, "crazy militants");
+        v = v.replace(/\bislamic militants\b/g, "crazy militants");
 	v = v.replace(/\bIslamic Militants\b/g, "Crazy Militants");
 	v = v.replace(/\bIslamic-Militants\b/g, "Crazy-Militants");
-	v = v.replace(/\bIslamic Militant\b/g, "Crazy Militant");
-	v = v.replace(/\bIslamic militants\b/g, "Crazy Militant");
+        v = v.replace(/\bIslamic militants\b/g, "Crazy Militant");
 
 
   textNode.nodeValue = v;
